d8da21b5407efba589935d94e285159a5c769043
charllie <charlie59320@gmail.com>
2015-06-25 16:15:49 +0200

    Fusiooon

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ app.config(['$routeProvider', function($routeProvider) {
 		})
 		.when('/sip', {
 			templateUrl: 'views/sip.html',
-			controller: 'UserCtrl'
+			controller: 'SIPCtrl'
 		})
 		.otherwise({
 			redirectTo: '/'
@@ -21,5 +21,48 @@ app.controller('UserCtrl', ['$scope', '$location', 'aiStorage', function($scope,
 	$scope.submit = function(user) {
 		store.set('user', user);
 		$location.path("/sip");
+	}
+}]);
+
+app.controller('SIPCtrl', ['$scope', 'aiStorage', function($scope, store) {
+	var user = store.get('user');
+	$scope.user = user;
+
+	$scope.session = null;
+
+	var endButton = document.getElementById('endCall').addEventListener("click", function() {
+		$scope.session.bye();
+		alert("Call Ended");
+	}, false);
+
+	var configuration = {
+	    uri: user.uri,
+	    authorizationUser: user.id,
+	    password: user.pwd,
+	    wsServers: user.wsServer,
+	    stunServers: ["stun:stun.l.google.com:19302"],
+	};
+
+	var options = {
+		media: {
+			constraints: {
+				audio: true,
+				video: false
+			},
+			render: {
+				remote: document.getElementById('remoteAudio'),
+				local: document.getElementById('localAudio')
+			}
+		}
+	};
+
+	var userAgent = new SIP.UA(configuration);
+
+	userAgent.on('invite', function(session) {
+		session.accept(option);
+	});
+
+	$scope.call = function(person) {
+		userAgent.invite('sip:' + person.id + '@' + user.server, options);
 	}
 }]);
