aa7d4cb43cf7ca5644984bae234ea213800750dd
charllie <charlie59320@gmail.com>
2015-07-03 18:43:39 +0200

    Kurento 6

--- kurento-agent/src/main/java/cz/cvut/fel/webrtc/GroupCallApp.java
+++ kurento-agent/src/main/java/cz/cvut/fel/webrtc/GroupCallApp.java
@@ -1,4 +1,18 @@
-package cz.cvut.fel;
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package cz.cvut.fel.webrtc;
 
 import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +33,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableAutoConfiguration
 public class GroupCallApp implements WebSocketConfigurer {
 
-	final static String DEFAULT_KMS_WS_URI = "ws://46.101.255.28:8888/kurento";
+	final static String DEFAULT_KMS_WS_URI = "ws://localhost:8888/kurento";
 
 	@Bean
 	public UserRegistry registry() {
