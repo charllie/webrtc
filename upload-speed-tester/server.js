4713300db7401486ca122d1de0b1fc074eacfeff
charllie <charlie59320@gmail.com>
2015-07-23 16:42:50 +0200

    Fix server

--- upload-speed-tester/server.js
+++ upload-speed-tester/server.js
@@ -8,8 +8,11 @@ var opts = {
 };
 
 app.use(express.static(__dirname + '/html'));
-app.use(express.limit(opts.maxUploadSize));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '20MB', extended: true }));
+
+app.get('/upload', function(req, res) {
+	res.end();
+});
 
 app.post('/upload', function(req, res) {
 	res.end();
