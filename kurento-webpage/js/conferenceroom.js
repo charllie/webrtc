4b7770b6de5b9748c43a26e6f0089970500069db
Thomas <thomasmarchal71@hotmail.fr>
2015-08-10 17:57:39 +0200

    Revert "conf file"
    This reverts commit c6ff92b41d186e9a04317492f1a45bcff04b25eb.

--- kurento-webpage/js/conferenceroom.js
+++ kurento-webpage/js/conferenceroom.js
@@ -12,16 +12,7 @@
  * Lesser General Public License for more details.
  *
  */
-messageResource.init({
-  // path to directory containing message resource files(.properties files),
-  // give empty string or discard this configuration if files are in the
-  // same directory as that of html file.
-  filePath : '/webrtc/kurento-agent/application.properties'
-});
-messageResource.load('config', function(){});
-var value = messageResource.get('client.ws', 'config');
-
-var ws = new WebSocket(value);
+var ws = new WebSocket('wss://webrtc.ml/groupcall');
 var inRoom = false;
 var participants = {};
 var name;
